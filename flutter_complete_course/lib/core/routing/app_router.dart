4162d585db6cbad3391f6a9e5616afc93864ee3f
Ahmed Osama <ahmed.osama.younes.dev@gmail.com>
2026-03-17 08:01:39 +0200

    - added  di - added error handler file - added constants file - added error model file - added api result - added login requst model - added login response model - added dio factory file
    - added api service file
    - added repo file
    - added cubit , state files
    - added login bloc listener
    - added ui to login ( email and password file)
    - added password validations file
    - added app regex

--- flutter_complete_course/lib/core/routing/app_router.dart
+++ flutter_complete_course/lib/core/routing/app_router.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_complete_course/core/di/depnedency_injaction.dart';
 import 'package:flutter_complete_course/core/routing/routes.dart';
+import 'package:flutter_complete_course/features/home/home_screen.dart';
+import 'package:flutter_complete_course/features/login/logic/cubit/login_cubit.dart';
 import 'package:flutter_complete_course/features/login/login_screen.dart';
 import 'package:flutter_complete_course/features/onboarding/onboarding_screen.dart';
 
@@ -9,7 +13,14 @@ class AppRouter {
       case Routes.onboardingScreen:
         return MaterialPageRoute(builder: (_) => OnboardingScreen());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => LoginScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
 
       default:
         return MaterialPageRoute(
--- flutter_complete_course/lib/core/routing/routes.dart
+++ flutter_complete_course/lib/core/routing/routes.dart
@@ -1,5 +1,6 @@
 class Routes {
   static const String onboardingScreen = '/onboarding_screen';
   static const String loginScreen = '/login_screen';
+  static const String homeScreen = '/home_screen';
   
 }
